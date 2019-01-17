cac7e772b2ac89753f9bc3d2920aa48d89f6a29b
Vasu Sharma <sharma.vasu55@gmail.com>
2019-01-17 04:01:49 +0000

    Draft 1 superpose attack ready

--- superpose_attack_batch.py
+++ superpose_attack_batch.py
@@ -12,7 +12,7 @@ import pdb
 
 import config_att_batch as config
 
-max_inter = 10
+max_inter = 1
 lr_halflife = config.lr_halflife
 initial_lr = config.init_lr
 lambda1_multiplier = 1
@@ -75,93 +75,102 @@ class Attacker:
         loss2: L1 sum of noise added 
         mean_noise: avg per pixel per channel noise
         '''
-        #a in targetted case is the target but untargetted case is ground truth ans
-
-        # im is torch tensor, converting everything into Variable
-        #img = Variable(img.cuda(async=True))
-        img_clone = img.clone()
+	img_clone1 = img1.clone()
+        img_clone2 = img2.clone()
         #que = Variable(torch.from_numpy(que).long().cuda(async=True))
         #ans = Variable(torch.from_numpy(ans).cuda(async=True))
         #que_len = Variable(torch.from_numpy(que_len).cuda(async=True))
 
-        ans_, att_, a_ = self.VQA_model.forward_pass(img, que, que_len)
+        #pdb.set_trace()        
+        ans_1, att_1, a_1 = self.VQA_model.forward_pass(img1, que1, que_len1)
+        a_new1 = F.softmax(a_1.view(a_1.size(0), a_1.size(1), -1), 2)
+        a_1 = a_new1.view(a_1.size(0), a_1.size(1), a_1.size(2), a_1.size(3))
 
-        self.targetted_const = 1
+        ans_2, att_2, a_2 = self.VQA_model.forward_pass(img2, que2, que_len2)
+        a_new2 = F.softmax(a_2.view(a_2.size(0), a_2.size(1), -1), 2)
+        a_2 = a_new2.view(a_2.size(0), a_2.size(1), a_2.size(2), a_2.size(3))
 
-        prob_value, ans_index = ans_.data.cpu().max(dim=1)
+        #pdb.set_trace()
+
+        prob_value1, ans_index1 = ans_1.data.cpu().max(dim=1)
         #ans stores the target index for the attack or ground truth for untargetted
-        _, target_idx = ans.data.cpu().max(dim=1)
-        '''
-        print(self.VQA_model.index_to_answer[target_idx.numpy()[0]],
-            self.VQA_model.index_to_answer[ans_index.numpy()[0]])
+        _, target_idx1 = ans1.data.cpu().max(dim=1)
+        _, target_idx2 = ans2.data.cpu().max(dim=1)
 
-        img_np1 = self.unorm(img.data).cpu().numpy()
-        img_cv = np.transpose(img_np1,(0,2,3,1))
-        img_cv = cv2.convertScaleAbs(img_cv.reshape(448,448,3)*255)
-        cv2.imshow('purturbed', img_cv)
-        cv2.waitKey(0)
-        '''
-        ####################################################################################
-        #This needs to be fixed to also do for targetted.
-        #Only untargetted for now
-        ############################################## Works only with batch_size = 1 for now. Modify for larger sizes later
-        #if (target_idx.numpy()[0] == ans_index.numpy()[0]):   #If actual VQA model is right then attack it else dont. #if actual answer same as predicted then attack it
-        orig, success, img_cv, loss1, loss2, mean_noise = self.perform(img, que, que_len, ans, total_iterations, True) ################################################################################################################################################ Check. Perform has grads. Not needed in val
-        return orig, success, img_cv, loss1, loss2, mean_noise #orig = True as original VQA model is right. success decided by attack. img is perturbed image
-        #return True, False, None 
+        #orig stores whether the original VQA model predicts the answer correctly
+        orig = (ans_index1 == target_idx1).numpy()
+
+        att_cpu1 = att_1.data.cpu().numpy()
+        #Break computational graph to not backprop into the VQA model itself
+        att_clone1 = Variable(torch.from_numpy(att_cpu1).cuda(), requires_grad= False).view(que.shape[0], 4096, 14, 14)
+
+        att_cpu2 = att_2.data.cpu().numpy()
+        #Break computational graph to not backprop into the VQA model itself
+        att_clone2 = Variable(torch.from_numpy(att_cpu2).cuda(), requires_grad= False).view(que.shape[0], 4096, 14, 14)
+        #####################################################################################################################################Initialize this to a array of False. Use this to zero out loss of entries where already success. Also use for premature exit if needed. Also remove image unNormalize and image reconstruction for now
+        #success = False
+        success = [False] * que.size(0) #Stores which samples in the batch have already been successfully attacked
+        iter_ = 0
+        img_cv = None
 
 
-        #Try replacing the above 2 lines with this:
-        '''
+        #Change this to allow for custom learning rates
+        #for param_group in self.optimizer.param_groups:
+        #    param_group['lr'] = config.init_lr
+        while (iter_ < max_inter):
+
+            iter_ += 1
+            #update_learning_rate(self.optimizer, total_iterations) #####################################Replace with scheduler instead using decrease_on_plateau
+
             #Attack the image now using attention maps
-            purturb = self.attack_model(att_)
+            joint_att = torch.cat((att_clone1, att_clone2), 1)
+            purturb = self.attack_model(joint_att)      #Could either be superposition weight map or noise map depending on what you are doing
+            update_map = Variable((torch.FloatTensor(success)).unsqueeze(1).unsqueeze(1).unsqueeze(1).expand_as(img_clone).cuda()) #Use success to construct a mask which prevents changes to perturbed images which succeed in attack
+            update_map.requires_grad = False
             #Add noise back to image
-            img_ = img_clone + purturb
-
+            if iter_ == 1:
+                img_ = (img_clone1 + purturb)
+            else:
+                img_ =  (1 - update_map) * (img_clone1 + purturb) + (update_map * img_) #Update only the images for which success is False
             #Get answer and attention maps when perturbed image is fed to the VQA network
-            ans_t, att_t = self.VQA_model.forward_pass(img_, que, que_len)
+            ans_t, att_t, a_t = self.VQA_model.forward_pass(img_, que, que_len)
 
-            #Compute added noise
-            mean_noise = torch.abs(purturb).mean()
-            noise = torch.abs(purturb).sum()  #Using L1 for now, see if you wanna use L2 instead.
+            #Compute targetted loss
+            nll = self.log_softmax(ans_t) # self.targetted_const is 1 if untargetted -1 if targetted
+            #ans in targetted case is the target but untargetted case is ground truth ans. (nll*ans) controls the correct loss
+            var_success = Variable(torch.FloatTensor(success).cuda())
+	    var_success.requires_grad = False
+            #pdb.set_trace()
+            if iter_ == 1:
+                loss1 = self.scaller_const * (nll*ans2).mean()
+                mean_noise = torch.abs(purturb).mean(dim=3).mean(dim=2).mean(dim=1)
+                loss2 = torch.abs(purturb).mean()  #Using L1 for now, see if you wanna use L2 instead.
+                loss = lambda1_multiplier * loss1 + lambda2_multiplier * loss2
+            else:
+                loss1 = ((self.scaller_const * ((nll*ans2).sum(dim=1) * (1 - var_success))).sum() / torch.sum((1 - var_success))) #Dont penalize samples on which success has already been obtained  
+                #Penalize large noise. Can modify to allow a certain extent of noise
+                mean_noise = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1) / torch.sum((1 - var_success))
+                loss2 = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1).sum() / torch.sum((1 - var_success))  #Penalize only non success cases #Using L1 for now, see if you wanna use L2 instead.
+                loss = lambda1_multiplier * loss1 + lambda2_multiplier * loss2
+
 
             # get answer index
             prob_value, ans_index = ans_t.data.cpu().max(dim=1)
-            _, target_idx = ans.data.cpu().max(dim=1)
+            _, target_idx2 = ans2.data.cpu().max(dim=1)
 
-            success = False
-
-            if self.targetted_const == 1:  
-
-                if(noise.data.cpu().numpy()[0] < 26000 and 
-                        (ans_index.numpy()[0] != target_idx.numpy()[0])): ###Check   #Condition that noise levels are thresholded and targetted attack is successful
-
-                    #print(self.VQA_model.index_to_answer[target_idx.numpy()[0]],
-                        self.VQA_model.index_to_answer[ans_index.numpy()[0]])
-
-                    img_np1 = self.unorm(img_.data).cpu().numpy()
-                    img_cv = np.transpose(img_np1,(0,2,3,1))
-                    img_cv = cv2.convertScaleAbs(img_cv.reshape(448,448,3)*255)
-                    img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-                    #cv2.imshow('purturbed', img_cv)
-                    #cv2.waitKey(0)
-
-                    success = True
-            else:
-                if(noise.data.cpu().numpy()[0] < 26000 and 
-                        (ans_index.numpy()[0] == target_idx.numpy()[0])): ###Check    #Condition that noise levels are thresholded and untargetted attack is successful
-                    img_np1 = self.unorm(img_.data).cpu().numpy()
-                    img_cv = np.transpose(img_np1,(0,2,3,1))
-                    img_cv = cv2.convertScaleAbs(img_cv.reshape(448,448,3)*255)
-                    #cv2.imshow('purturbed', img_cv)
-                    #cv2.waitKey(100)
-                    success = True
+            success = np.logical_or(success, np.logical_and(loss2.data.cpu().numpy() < 0.26000, (ans_index.numpy() == target_idx2.numpy())))
+            if np.sum(success) == success.shape[0]:
+                break
 
 
-            return True, success, img_cv, None, noise, mean_noise
-        '''
-        #else:
-            #return False, False, None, None, None, None #orig = False as the original VQA model itself misclassifies
+        img_np1 = self.unorm(img_.data).cpu().numpy()
+        img_cv = np.transpose(img_np1,(0,2,3,1))
+        img_cv = cv2.convertScaleAbs(img_cv.reshape(448,448,3)*255)
+
+        return orig, success, img_cv, loss1, loss2, mean_noise
+
+
+
 
     def perform(self, img1, img2, que1, que2, ans1, ans2, que_len1, que_len2, total_iterations, val=False):
         '''
@@ -225,7 +234,7 @@ class Attacker:
         #Change this to allow for custom learning rates
         #for param_group in self.optimizer.param_groups:
         #    param_group['lr'] = config.init_lr
-        while (iter_ < max_inter):
+        while (iter_ < max_iter): #Setting max_iter to 1 for now. Change to allow for multi step attack
 
             iter_ += 1
             #update_learning_rate(self.optimizer, total_iterations) #####################################Replace with scheduler instead using decrease_on_plateau
@@ -244,16 +253,22 @@ class Attacker:
             ans_t, att_t, a_t = self.VQA_model.forward_pass(img_, que, que_len)
 
             #Compute targetted loss
-            nll = -1 * self.targetted_const * self.log_softmax(ans_t) # self.targetted_const is 1 if untargetted -1 if targetted
+            nll = self.log_softmax(ans_t) # self.targetted_const is 1 if untargetted -1 if targetted
             #ans in targetted case is the target but untargetted case is ground truth ans. (nll*ans) controls the correct loss
             var_success = Variable(torch.FloatTensor(success).cuda())
             var_success.requires_grad = False
             #pdb.set_trace()
-            loss1 = ((self.scaller_const * ((nll*ans).sum(dim=1) * (1 - var_success))).sum() / torch.sum((1 - var_success))) #Dont penalize samples on which success has already been obtained  
-            #Penalize large noise. Can modify to allow a certain extent of noise
-            mean_noise = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1) / torch.sum((1 - var_success))
-            loss2 = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1).sum() / torch.sum((1 - var_success))  #Penalize only non success cases #Using L1 for now, see if you wanna use L2 instead.
-            loss = lambda1_multiplier * loss1 + lambda2_multiplier * loss2
+	    if iter_ == 1:
+		loss1 = self.scaller_const * (nll*ans2).mean()
+                mean_noise = torch.abs(purturb).mean(dim=3).mean(dim=2).mean(dim=1)
+                loss2 = torch.abs(purturb).mean()  #Using L1 for now, see if you wanna use L2 instead.
+                loss = lambda1_multiplier * loss1 + lambda2_multiplier * loss2
+	    else:
+            	loss1 = ((self.scaller_const * ((nll*ans2).sum(dim=1) * (1 - var_success))).sum() / torch.sum((1 - var_success))) #Dont penalize samples on which success has already been obtained  
+            	#Penalize large noise. Can modify to allow a certain extent of noise
+            	mean_noise = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1) / torch.sum((1 - var_success))
+            	loss2 = torch.abs((1 - update_map) * purturb).mean(dim=3).mean(dim=2).mean(dim=1).sum() / torch.sum((1 - var_success))  #Penalize only non success cases #Using L1 for now, see if you wanna use L2 instead.
+            	loss = lambda1_multiplier * loss1 + lambda2_multiplier * loss2
 
 	    if not val:
             	self.optimizer.zero_grad()
@@ -262,45 +277,11 @@ class Attacker:
 
             # get answer index
             prob_value, ans_index = ans_t.data.cpu().max(dim=1)
-            _, target_idx = ans.data.cpu().max(dim=1)
+            _, target_idx2 = ans2.data.cpu().max(dim=1)
 
-            if self.targetted_const == -1:
-                ############################################## Works only with batch_size = 1 for now. Modify for larger sizes later
-                #if(loss2.data.cpu().numpy()[0] < 26000 and (ans_index.numpy()[0] != target_idx.numpy()[0])): ###Check   #Condition that noise levels are thresholded and targetted attack is successful
+            success = np.logical_or(success, np.logical_and(loss2.data.cpu().numpy() < 0.26000, (ans_index.numpy() == target_idx2.numpy())))
+            if np.sum(success) == success.shape[0]:
+            	break
+        
 
-                    #print(self.VQA_model.index_to_answer[target_idx.numpy()[0]], self.VQA_model.index_to_answer[ans_index.numpy()[0]])
-
-                    #img_np1 = self.unorm(img_.data).cpu().numpy()
-                    #img_cv = np.transpose(img_np1,(0,2,3,1))
-                    #img_cv = cv2.convertScaleAbs(img_cv.reshape(img_cv.shape[0],448,448,3)*255)
-                    #img_cv = cv2.cvtColor(img_cv, cv2.COLOR_BGR2RGB)
-                    #cv2.imshow('purturbed', img_cv)
-                    #cv2.waitKey(0)
-                    #cv2.imshow('purturbed', img_cv)
-                    #cv2.waitKey(100)
-
-                    #success = True
-                    #break
-
-                success = np.logical_or(success, np.logical_and(loss2.data.cpu().numpy() < 0.26000, (ans_index.numpy() == target_idx.numpy())))
-                if np.sum(success) == success.shape[0]:
-                        break
-            else:
-                #if(loss2.data.cpu().numpy()[0] < 26000 and (ans_index.numpy()[0] == target_idx.numpy()[0])): ###Check    #Condition that noise levels are thresholded and untargetted attack is successful
-                    #img_np1 = self.unorm(img_.data).cpu().numpy()
-                    #img_cv = np.transpose(img_np1,(0,2,3,1))
-                    #img_cv = cv2.convertScaleAbs(img_cv.reshape(img_cv.shape[0], 448,448,3)*255)
-                    #cv2.imshow('purturbed', img_cv)
-                    #cv2.waitKey(100)
-                    #success = True
-                    #break
-
-
-                success = np.logical_or(success, np.logical_and(loss2.data.cpu().numpy() < 0.26000, (ans_index.numpy() != target_idx.numpy())))
-                if np.sum(success) == success.shape[0]:
-			break
-            #print(prob_value, ans_index, target_idx, loss2.data.cpu().numpy(),  loss1.data.cpu().numpy())
-
-
-            #print(loss2, loss1)
-        return orig, success, None, loss1, loss2, mean_noise
+	return orig, success, None, loss1, loss2, mean_noise
